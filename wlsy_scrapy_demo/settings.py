75685057ae9ffc9370aa882b93945c4fbbef35d4
silencewwt <silencewwt@gmail.com>
2015-03-11 12:06:20 +0800

    init

---
+++ wlsy_scrapy_demo/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for wlsy_scrapy_demo project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'wlsy_scrapy_demo'
+
+SPIDER_MODULES = ['wlsy_scrapy_demo.spiders']
+NEWSPIDER_MODULE = 'wlsy_scrapy_demo.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'wlsy_scrapy_demo (+http://www.yourdomain.com)'
